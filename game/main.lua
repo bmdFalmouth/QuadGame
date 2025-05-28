8e2ee4a0be6a2b28733902061ec6be7cd6965c76
Brian McDonald <brian.mcdonald@falmouth.ac.uk>
2025-05-28 10:02:48 +0100

    modified the main to use new game screens

--- game/main.lua
+++ game/main.lua
@@ -1,3 +1,5 @@
+require "game"
+
 --960
 --540
 TopLeftCanvas=nil
@@ -7,6 +9,8 @@ BottomRrightCanvas=nil
 
 
 Player=nil
+CurretGame=nil
+Games={}
 
 GamePosition={top_left=1,top_right=2,bottom_left=3,bottom_right=4}
 CurrentGamePosition=GamePosition.top_left
@@ -16,9 +20,6 @@ BottomLeftRect={x=0,y=540,w=960,h=540}
 TopRightRect={x=960,y=0,w=960,h=540}
 BottomRightRect={x=960,y=540,w=960,h=540}
 
-GameText="Top Left Game"
-
-
 function love.load()
     love.window.setMode(1920, 1080, {fullscreen = false, vsync = true})
     love.window.setPosition(40, 40)
@@ -27,66 +28,48 @@ function love.load()
     TopLeftCanvas = love.graphics.newCanvas(TopLeftRect.w, TopLeftRect.h)
     BottomLeftCanvas = love.graphics.newCanvas(BottomRightRect.w, BottomRightRect.h)
 
-    Player = {x=100,y=300,width=50,height=50}
+    Player = {x=100,y=300,width=50,height=50,moveSpeed=100,
+    
+    move=function (self, dx, dy,dt)
+        self.x = self.x + (dx*self.moveSpeed * dt)
+        self.y = self.y + (dy*self.moveSpeed * dt)
+        
+    end
+    }
+    topLeftGame = GameScreen:new(nil, "Top Left Game", TopLeftCanvas, TopLeftRect)
+    bottomLeftGame = GameScreen:new(nil, "Bottom Left Game", BottomLeftCanvas, BottomLeftRect)
+    topRightGame = GameScreen:new(nil, "Top Right Game", TopRightCanvas, TopRightRect)
+    bottomRightGame = GameScreen:new(nil, "Bottom Right Game", BottomRrightCanvas, BottomRightRect)
+    Games={topLeftGame, bottomLeftGame, topRightGame, bottomRightGame}
+    CurrentGame=Games[CurrentGamePosition]
 end
 
 function love.update(dt)
     -- Update logic here
     if love.keyboard.isDown("up") then
-        Player.y = Player.y - 100 * dt
+        --Player.y = Player.y - Player.moveSpeed * dt
+        Player:move(0, -1, dt)
     end
     if love.keyboard.isDown("down") then
-        Player.y = Player.y + 100 * dt
+        Player:move(0, 1, dt)
     end
     if love.keyboard.isDown("left") then
-        Player.x = Player.x - 100 * dt
+        Player:move(-1, 0, dt)
     end
     if love.keyboard.isDown("right") then
-        Player.x = Player.x + 100 * dt
+        Player:move(1, 0, dt)
     end
 
-    --check where the player is
-    if Player.x < TopLeftRect.w and Player.y < TopLeftRect.h then
-        CurrentGamePosition = GamePosition.top_left
-        GameText="Top Left Game"
-    elseif Player.x < TopLeftRect.w and Player.y > TopLeftRect.h then
-        CurrentGamePosition = GamePosition.bottom_left
-        GameText="Bottom Left Game"
-    elseif Player.x > TopLeftRect.w and Player.y < TopRightRect.h then
-        CurrentGamePosition = GamePosition.top_right
-        GameText="Top Right Game"
-    elseif Player.x > TopRightRect.w and Player.y > TopLeftRect.h then
-        CurrentGamePosition = GamePosition.bottom_right
-        GameText="Bottom Right Game"
-    end
+    CurrentGame:update(dt)
 end
 
 function love.draw()
-    love.graphics.setCanvas(TopRightCanvas)
-    love.graphics.clear(1, 0, 0, 1)
-
-    love.graphics.setCanvas(BottomRrightCanvas)
-    love.graphics.clear(0, 1, 0, 1)
-    love.graphics.setColor( 0, 0, 0 )
-
-    love.graphics.setCanvas(TopLeftCanvas)
-    love.graphics.clear(0, 0, 1, 1)
-    love.graphics.setColor( 0, 0, 0 )
-    
-    love.graphics.setCanvas(BottomLeftCanvas)
-    love.graphics.clear(1, 1, 0, 1)
-    love.graphics.setColor( 0, 0, 0 )
-    
 
     love.graphics.setCanvas()
     love.graphics.clear(0, 0, 0, 1)
-    love.graphics.setColor( 1, 1, 1 )
-    love.graphics.draw(TopRightCanvas, TopRightRect.x, TopRightRect.y)
-    love.graphics.draw(BottomRrightCanvas, BottomRightRect.x, BottomRightRect.y)
-    love.graphics.draw(TopLeftCanvas, TopLeftRect.x, TopLeftRect.y)
-    love.graphics.draw(BottomLeftCanvas, BottomLeftRect.x, BottomLeftRect.y)
     love.graphics.setColor( 0, 0, 0 )
+    CurrentGame:draw()
     love.graphics.rectangle("fill", Player.x, Player.y, Player.width, Player.height)
-    love.graphics.print(GameText, 30, 30)
+    love.graphics.print(CurrentGame.name, 30, 30)
 
 end
